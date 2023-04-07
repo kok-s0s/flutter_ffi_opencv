2cc042a48ba418b821886fd982de330660cf3f4f
kok-s0s <kok_s0s@163.com>
2023-04-07 17:00:10 +0800

    chore: generated file

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_opencv/flutter_opencv_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_opencv_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterOpencvPlugin");
+  flutter_opencv_plugin_register_with_registrar(flutter_opencv_registrar);
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_opencv
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterOpencvPlugin.register(with: registry.registrar(forPlugin: "FlutterOpencvPlugin"))
+}
